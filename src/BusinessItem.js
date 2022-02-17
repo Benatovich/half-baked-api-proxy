40f96863f12397d88136d59a24376a4dc13e8050
Benatovich <sambenatovich@gmail.com>
2022-02-16 19:02:49 -0500

    yelp function works! doing a little styling and testing next

--- src/BusinessItem.js
+++ src/BusinessItem.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-export default function BusinessItem({ name, display_phone, image_url, price, rating, url }) {
+export default function BusinessItem({ name, display_phone, image_url, price, rating, url, categories }) {
   return (
     <div className='business-item'>
       <h3 className='name'>{name}</h3>
-      <p>{image_url}</p>
-      <p>{price}</p>
-      <p>{rating}</p>
+      <img className='image' src={image_url} />
+      {/* <p>{[categories]}</p> */}
+      <p>Price: {price}</p>
+      <p>Rating: {rating}</p>
       <p>{display_phone}</p>
-      <p>{url}</p>
+      {/* <a href={url} /> */}
     </div>
   );
 }
--- src/BusinessesList.js
+++ src/BusinessesList.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import BusinessItem from './BusinessItem';
 
-export default function BusinessesList() {
+export default function BusinessesList(props) {
+  const businessesEl = props.businesses.map((item, i) => {
+    return (
+      <BusinessItem key={`${item.name} + ${i}`} name={item.name} display_phone={item.display_phone} image_url={item.image_url} price={item.price} rating={item.rating} url={item.url} categories={item.categories} />
+    );
+  });
   return (
-    <div>BusinessesList</div>
+    <div>{businessesEl}</div>
   );
 }
